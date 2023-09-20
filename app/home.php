7aec64a21aec5345681a6f80138432dc68d36151
iluvshi <shian.morrison@gmail.com>
2023-09-19 20:29:10 -0400

    init front end components

---
+++ app/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+      <div class="welcome">
+          <h1 style="color:black">Welcome (user) </h1>
+      </div>
+      <div class="outline" style="margin-top: 100px;">
+        <div class="input-box">
+          <label style="font-weight: thin;" for="Search">Search Contact</label>
+          <input style="width:fit-content">
+        </div>
+      </div>
+  </body>
+</html>
